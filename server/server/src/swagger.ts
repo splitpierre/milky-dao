dc0baa2fddd4992b119664415917febee7a19f96
Pierre Maciel <pierre@webizz.biz>
2022-09-17 16:44:14 -0300

    swagger update

--- server/server/src/swagger.ts
+++ server/server/src/swagger.ts
@@ -3,9 +3,9 @@ import { DocumentBuilder } from "@nestjs/swagger";
 export const swaggerPath = "api";
 
 export const swaggerDocumentOptions = new DocumentBuilder()
-  .setTitle("Milky Dao")
+  .setTitle("Milky DAO API")
   .setDescription(
-    '\n\n## Congratulations! Your service resource is ready.\n  \nPlease note that all endpoints are secured with JWT Bearer authentication.\nBy default, your service resource comes with one user with the username "admin" and password "admin".\nLearn more in [our docs](https://docs.amplication.com)'
+    '\n\n## Welcome to Milky DAO API!\n  \nPlease note that all endpoints are secured with JWT Bearer authentication.\nBy default, your service resource comes with one user with the username "admin" and password "admin".\nLearn more in [our repo](https://github.com/splitpierre/milky-dao)'
   )
   .setVersion("novb0pjs")
   .addBearerAuth()
@@ -17,5 +17,5 @@ export const swaggerSetupOptions = {
   },
   customCssUrl: "../swagger/swagger.css",
   customfavIcon: "../swagger/favicon.png",
-  customSiteTitle: "Milky Dao",
+  customSiteTitle: "Milky DAO API",
 };

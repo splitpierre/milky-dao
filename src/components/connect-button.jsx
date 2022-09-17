76c185ef0c2bbb3df707d872bb7220121b1b292d
Pierre Maciel <pierre@webizz.biz>
2022-09-17 18:57:53 -0300

    misc visual adj.

---
+++ src/components/connect-button.jsx
@@ -0,0 +1,18 @@
+
+
+const ConnectButton = () => {
+  
+    return (
+        <button
+        type="button"
+        id="open-connect-button"
+        class="inline-flex btn rounded-md text-sm p-2 font-bold"
+        aria-label="Connect"
+      >
+         Connect
+      </button>
+    );
+  };
+  
+  // render(SolidAPI, document.getElementById("app"));
+  export default ConnectButton;

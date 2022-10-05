f7da01f0f3df9e977bab7d2995dd91a7ce94527a
Pierre Maciel <pierre@webizz.biz>
2022-10-05 10:27:39 -0300

    astro static>ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://milky-dao.vercel.app/",
+  output: "server",
   integrations: [tailwind(), solid(), image(), mdx()],
   vite: {
     ssr: {

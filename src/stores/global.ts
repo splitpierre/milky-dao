e0a546e4bd96cf805eefbfc283c580af36779ec1
Pierre Maciel <pierre@webizz.biz>
2022-10-05 11:09:29 -0300

    api_url fix

--- src/stores/global.ts
+++ src/stores/global.ts
@@ -1,8 +1,5 @@
-export const STORE_ENV: any = "production";
+// export const STORE_ENV: any = "local";
 
 export const STORE_API = {
-  api_url:
-    STORE_ENV === "production"
-      ? "https://milky-dao-api.herokuapp.com"
-      : "http://localhost:3005",
+  api_url: "https://milky-dao-api.herokuapp.com", // http://localhost:3005
 };
